396a6e4f118b29d502bb7292e56d30f7301a2e98
ddave592 <david.g1591@googlemail.com>
2019-11-15 13:24:09 +0000

    final-fcc-commit-no-history-state

--- src/App.js
+++ src/App.js
@@ -2,85 +2,60 @@ import React, { useState, useEffect } from 'react';
 import './styles/index.scss';
 import ReactFCCtest from 'react-fcctest';
 import getQuote from './utils/quoteHandler';
-import twitterIcon from './icons/twitter-social-circle-color.png';
-
-
-console.log(getQuote().quoteText);
-
-const QuoteBox = (props) => {
-
-  return (
-    <div className={"quote-card " + props.cName} id="quote-box">
-      <blockquote>
-        <p id='text'>
-          {props.quote}
-        </p>
-        <footer id="author">
-          <em>
-            {props.author}
-          </em>
-        </footer>
-      </blockquote>
-      <div className="button-group">
-        <button id='new-quote' className='btn-outline btn-0' onClick={props.onClick}>New Quote!</button>
-        <a className='tweet' href={`https://www.twitter.com/intent/tweet?text='${props.quote.split(' ').reduce((previous, current) => previous + '+' + current)}'%20${props.author}`} id='tweet-quote'><img width="35px" src={twitterIcon} /></a>
-      </div>
-    </div>
-  );
-}
-
+import { QuoteBox } from './components/QuoteBox';
 
 const App = () => {
-  console.log(getQuote());
-  const [quote, setQuote] = useState('Loading...');
-  const [quoteAuthor, setQuoteAuthor] = useState('Loading...');
 
-  let pastQuoteArr = [{
-    "quoteAuthor": "Thomas Edison",
-    "quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration."
-  },
-  {
-    "quoteAuthor": "Yogi Berra",
-    "quoteText": "You can observe a lot just by watching."
-  },
-  {
-    "quoteAuthor": "Abraham Lincoln",
-    "quoteText": "A house divided against itself cannot stand."
-  }]
+  const [quote, setQuote] = useState({ quoteText: 'Loading...', quoteAuthor: 'Loading...' });
+  // const [pastQuote, setPastQuote] = useState([]);
+
+
+  // let pastQuoteArr = [{
+  //   "quoteAuthor": "Thomas Edison",
+  //   "quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration."
+  // },
+  // {
+  //   "quoteAuthor": "Yogi Berra",
+  //   "quoteText": "You can observe a lot just by watching."
+  // },
+  // {
+  //   "quoteAuthor": "Abraham Lincoln",
+  //   "quoteText": "A house divided against itself cannot stand."
+  // }]
 
   useEffect(() => {
     let quoteObj = getQuote();
-    setQuote(
-      quoteObj.quoteText
-    );
-    setQuoteAuthor(
-      '- ' + quoteObj.quoteAuthor
-    );
+    if (!quoteObj.quoteAuthor) {
+      quoteObj.quoteAuthor = 'Unknown'
+    };
+    setQuote(quoteObj);
   }, []);
 
 
   const handleClick = () => {
     let quoteObj = getQuote();
+    // setPastQuote(pastQuote + {
+    //   'quoteText': quote.quoteText,
+    //   'quoteAuthor': quote.quoteAuthor
+    // });
     if (!quoteObj.quoteAuthor) {
       quoteObj.quoteAuthor = 'Unknown'
     };
     setQuote(
-      quoteObj.quoteText
-    );
-    setQuoteAuthor(
-      '- ' + quoteObj.quoteAuthor
+      quoteObj
     );
   }
 
-  console.log(quote);
   return (
     <>
       <nav>
-        <h1 className="title">Quote Generator.</h1>
+        <div className="title-container">
+          <h1 class="title">Quote Generator.</h1>
+        </div>
       </nav>
       <ReactFCCtest />
-      <QuoteBox id='quote-box' quote={quote} author={quoteAuthor} onClick={handleClick} />
-      {[...pastQuoteArr].map(x => <QuoteBox cName='past-quote' quote={x.quoteText} author={x.quoteAuthor} onClick={handleClick} />)}
+      <QuoteBox id='quote-box' quote={quote} onClick={handleClick} />
+      {/* {pastQuote ? [...pastQuote].map(x => <QuoteBox cName='past-quote' quote={x.quoteText} author={x.quoteAuthor} onClick={handleClick} />) : null} */}
     </>
   );
 }
---
+++ src/components/QuoteBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import twitterIcon from '../icons/twitter-social-circle-color.png';
+
+
+
+export const QuoteBox = (props) => {
+  return (<div className={"quote-card " + props.cName} id="quote-box">
+    <blockquote>
+      <p id='text'>
+        {props.quote.quoteText}
+      </p>
+      <footer id="author">
+        <em>
+          {'- ' + props.quote.quoteAuthor}
+        </em>
+      </footer>
+    </blockquote>
+    <div className="button-group">
+      <button id='new-quote' className='btn-outline btn-0' onClick={props.onClick}>New Quote!</button>
+      <a className='tweet'  href={`https://www.twitter.com/intent/tweet?text='${props.quote.quoteText.split(' ').reduce((previous, current) => previous + '+' + current)}'%20${'- ' + props.quote.quoteAuthor}`} id='tweet-quote'><img width="35px" alt="Twitter Icon" src={twitterIcon} /></a>
+    </div>
+  </div>);
+};

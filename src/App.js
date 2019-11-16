b0aa6e0e6a82a61d77fe8c994165ecdb4e0c51fe
ddave592 <david.g1591@googlemail.com>
2019-11-16 23:08:25 +0000

    implemented 3 quote history limit + title animation + styling restructure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { useState, useEffect } from 'react';
 import './styles/index.scss';
 import getQuote from './utils/quoteHandler';
 import { QuoteBox } from './components/QuoteBox';
+import { useTrail, animated } from 'react-spring'
 
 const App = () => {
 
+  //init states
   const [quote, setQuote] = useState({ quoteText: 'Loading...', quoteAuthor: 'Loading...' });
   const [pastQuote, setPastQuote] = useState([]);
 
+
+  //animation
+  const items = ['Quote', ' Generator.']
+  const config = { mass: 5, tension: 1000, friction: 100 }
+
+  const trail = useTrail(items.length, {
+    config,
+    opacity: 1,
+    x: 0,
+    height: 80,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+
+
+  //hooks
   useEffect(() => {
     let quoteObj = getQuote();
     if (!quoteObj.quoteAuthor) {
@@ -15,12 +32,20 @@ const App = () => {
     };
     setQuote(quoteObj);
   }, []);
-
+console.log(pastQuote.length);
 
   const handleClick = () => {
     let quoteObj = getQuote();
-    setPastQuote([...pastQuote, {...quote}]
-    );
+    console.log(pastQuote.length);
+    if (pastQuote.length < 3) {
+      setPastQuote([...pastQuote, { ...quote }]
+      );
+    } else {
+      let previousPastQuote = [...pastQuote];
+      previousPastQuote.shift();
+      setPastQuote([...previousPastQuote, { ...quote }]
+      )
+    }
     if (!quoteObj.quoteAuthor) {
       quoteObj.quoteAuthor = 'Unknown'
     };
@@ -29,17 +54,33 @@ const App = () => {
     );
   }
 
+  let historyClasses = pastQuote.length === 0 ? 'history-title' : 'history-title display'
+  const pastQuoteMap = pastQuote ? [...pastQuote].reverse().map((x) => {
+    return <QuoteBox cName='past-quote' key={x.id} quote={x} buttons={false} />
+  }) : null;
 
-//       to-dos:
-// - add transition animations to new main-quote-container components + make current-quote unmount and have new one mount to control transitions
-// - have history components animate down as new ones are added to array
-// restructure div containers
+
+  //       to-dos:
+  // - add transition animations to new main-quote-container components + make current-quote unmount and have new one mount to control transitions
+  // - have history components animate down as new ones are added to array
+  // - add react-springs for history components
+
+
 
   return (
     <>
       <nav>
         <div className="title-container">
-          <h1 className="title">Quote Generator.</h1>
+          <div className="h1-container">
+            {trail.map(({ x, height, ...rest }, index) => (
+              <animated.h1
+                className="title"
+                key={items[index]}
+                style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+                {items[index]}
+              </animated.h1>
+            ))}
+          </div>
         </div>
       </nav>
 
@@ -48,10 +89,10 @@ const App = () => {
           <QuoteBox id='quote-box' quote={quote} onClick={handleClick} buttons={true} />
         </div>
         <div className='history-quote-container'>
-          <h2 className='history-title'>
+          <h2 className={historyClasses}>
             Past Quotes:
         </h2>
-          {pastQuote ? [...pastQuote].reverse().map((x) => <QuoteBox cName='past-quote' key={x.id} quote={x} buttons={false} />) : null}
+          {pastQuoteMap}
         </div>
       </div>
     </>
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -9,44 +9,57 @@ export const QuoteBox = (props) => {
     : 'quote-card';
 
   if (props.buttons) {
-    return (<div className={classes} id="quote-box">
-      <blockquote>
-        <p id='text'>
-          {props.quote.quoteText}
-        </p>
-        <footer id="author">
-          <em>
-            {'- ' + props.quote.quoteAuthor}
-          </em>
-        </footer>
-      </blockquote>
-      <div className="button-group">
-        <button id='new-quote' className='btn-outline btn-0' onClick={props.onClick}>New Quote!</button>
-        <a
-          className='tweet'
-          href={`https://www.twitter.com/intent/tweet?text='${props.quote.quoteText.split(' ').reduce((previous, current) => previous + '+' + current)}'%20${'- ' + props.quote.quoteAuthor}`}
-          id='tweet-quote'>
-          <span className="tweet-text">
-            Tweet it
+    // main quote boxes
+    return (
+      <div className={classes} id="quote-box">
+        <blockquote>
+          <p id='text'>
+            {props.quote.quoteText}
+          </p>
+          <footer id="author">
+            <em>
+              {'- ' + props.quote.quoteAuthor}
+            </em>
+          </footer>
+        </blockquote>
+        <div className="button-group">
+          <button id='new-quote' className='btn-outline btn-0' onClick={props.onClick}>New Quote!</button>
+          <a
+            className='tweet'
+            href={`https://www.twitter.com/intent/tweet?text='${props.quote.quoteText
+              .split(' ')
+              .reduce((previous, current) => previous + '+' + current)}'%20${'- ' + props.quote.quoteAuthor}`}
+            id='tweet-quote'>
+            <span className="tweet-text">
+              Tweet it
           </span>
-          <img width="35px" alt="Twitter Icon" src={twitterIcon} />
-        </a>
+            <img width="35px" alt="Twitter Icon" src={twitterIcon} />
+          </a>
+        </div>
       </div>
-    </div>);
+    );
   } else {
-    return (<div className={classes} id="quote-box">
-      <blockquote>
-        <p id='text'>
-          {props.quote.quoteText}
-        </p>
-        <footer id="author">
-          <em>
-            {'- ' + props.quote.quoteAuthor}
-          </em>
-        </footer>
-      </blockquote>
-      <div className="button-group">
-      </div>
-    </div>)
+    // history quote boxes
+    return (
+      <a
+        className='box-anchor'
+        href={`https://www.twitter.com/intent/tweet?text='${props.quote.quoteText
+          .split(' ')
+          .reduce((previous, current) => previous + '+' + current)}'%20${'- ' + props.quote.quoteAuthor}`}
+      >
+        <div className={classes} id="quote-box">
+          <blockquote>
+            <p id='text'>
+              {props.quote.quoteText}
+            </p>
+            <footer id="author">
+              <em>
+                {'- ' + props.quote.quoteAuthor}
+              </em>
+            </footer>
+          </blockquote>
+        </div>
+      </a>
+    )
   }
 };
